7501bcd7461ca939ee77da2265c4e0847bcb782a
Wotever <nguilb@gmail.com>
2022-04-18 21:51:10 +0200

    Init : removing unused setting control

--- Payload/MotionAndDeviceRelated.cs
+++ Payload/MotionAndDeviceRelated.cs
@@ -1,14 +1,14 @@
 ﻿using GameReaderCommon;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SimHub.MQTTPublisher.Payload
 {
     public class MotionAndDeviceRelated
     {
-        
         public MotionAndDeviceRelated(GameData data)
         {
-            
+            mOrientation = data.NewData.CarCoordinates.ToList();
         }
 
         public List<double> mOrientation { get; set; }
