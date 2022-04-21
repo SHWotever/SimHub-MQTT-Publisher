3f09d4423e2d4315e6e2a9be5038bf7a978c5460
Wotever <nguilb@gmail.com>
2022-04-21 19:41:11 +0200

    New car and track data

--- Payload/PayloadRoot.cs
+++ Payload/PayloadRoot.cs
@@ -1,15 +1,10 @@
 ﻿using GameReaderCommon;
 using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimHub.MQTTPublisher.Payload
 {
-
     public class PayloadRoot
     {
-        
         public PayloadRoot(GameData data, SimHubMQTTPublisherPluginUserSettings userSettings)
         {
             time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
@@ -17,13 +12,21 @@ namespace SimHub.MQTTPublisher.Payload
             carState = new CarState(data);
             motionAndDeviceRelated = new MotionAndDeviceRelated(data);
             UserId = userSettings.UserId.ToString();
-
+            GameName = data.GameName;
         }
 
         public long time { get; set; }
+
         public string UserId { get; set; }
+
+        public string GameName { get; set; }
+
         public VehicleInformation vehicleInformation { get; set; }
+
         public CarState carState { get; set; }
+
         public MotionAndDeviceRelated motionAndDeviceRelated { get; set; }
+
+        public TrackInformation trackInformation { get; set; }
     }
 }
